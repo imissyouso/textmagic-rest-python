1086ac1f172703cb07043d02906f993b9a0bb522
Andrey Vorobyev <andrey.v@textmagic.biz>
2019-09-17 11:37:02 +0000

    Auto commit

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "TextMagic"
-VERSION = "2.0.262"
+VERSION = "2.0.265"
 # To install the library, run the following
 #
 # python setup.py install
