1c5aeaf1f76d54490c82ac7a7c721d9c4f577c90
Andrey Vorobyev <vorobivka@mail.ru>
2019-09-18 10:49:52 +0000

    Add C# SDK step

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "TextMagic"
-VERSION = "2.0.300"
+VERSION = "2.0.301"
 # To install the library, run the following
 #
 # python setup.py install
