8866a7b41e712a96455b7ee152104c9a3a5eff4c
Andrey Vorobyev <vorobivka@mail.ru>
2019-09-19 09:54:40 +0000

    Add C# sdk readme template

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "TextMagic"
-VERSION = "2.0.305"
+VERSION = "2.0.306"
 # To install the library, run the following
 #
 # python setup.py install
