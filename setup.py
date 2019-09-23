8eb34310e9c1e2d0411713c957399562168b2cdf
Andrey Vorobyev <vorobivka@mail.ru>
2019-09-23 13:03:08 +0000

    Change readme of perl

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "TextMagic"
-VERSION = "2.0.334"
+VERSION = "2.0.335"
 # To install the library, run the following
 #
 # python setup.py install
