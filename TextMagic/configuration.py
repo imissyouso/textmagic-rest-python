75f86134020bb8d7aeac140ca1c0f572cbf9e5ce
Andrey Vorobyev <vorobivka@mail.ru>
2019-09-24 11:53:32 +0000

    Fix PHP readme

--- TextMagic/configuration.py
+++ TextMagic/configuration.py
@@ -240,5 +240,5 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 2\n"\
-               "SDK Package Version: 2.0.359".\
+               "SDK Package Version: 2.0.360".\
                format(env=sys.platform, pyversion=sys.version)
