5cc9c6eda36a5e5d96c76ae8ffddedbb46809d44
Andrey Vorobyev <vorobivka@mail.ru>
2019-09-24 08:15:15 +0000

    Fix Go readme

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "TextMagic"
-VERSION = "2.0.339"
+VERSION = "2.0.340"
 # To install the library, run the following
 #
 # python setup.py install
